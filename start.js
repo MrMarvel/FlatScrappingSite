fa99ca0296a2f5ba2c0221d009e4be0b949932e0
MrMarvel <seregakkk999@yandex.ru>
2024-02-28 03:45:12 +0300

    init

---
+++ start.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8414
+const folder = '/home/sergey/FlatScrapping'
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+var serveIndex = require('serve-index');
+
+app.use('/flat-scrapping', serveIndex(folder,
+    option={'filter': (name, index, files, dir) => {
+            return files[index].includes('.csv');
+        }}));
